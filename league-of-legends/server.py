cd760292fa75b43d54faa71ebcc0f759f8950cb7
Toby Liu <ybot1122@gmail.com>
2025-06-21 20:56:18 -0700

    inprogress - get summoner match history

--- league-of-legends/server.py
+++ league-of-legends/server.py
@@ -63,6 +63,46 @@ async def get_items(championName: str) -> str:
 
 
 @mcp.tool()
+async def lookup_summoner(championName: str) -> str:
+  """Looks up the summoner for the given champion in the current game and returns stats/info about their match history.
+  Args:
+      championName (str): The name of the champion.
+  """
+
+  url = "https://127.0.0.1:2999/liveclientdata/allgamedata"
+
+  try:
+    async with httpx.AsyncClient(verify=False) as client:
+        resp = await client.get(url)
+        data = resp.json()
+
+    if not data:
+        return "No data found"
+    
+    currPlayer = next((p for p in data.get("allPlayers", []) if p.get("championName", "") == championName), None)
+    if not currPlayer:
+        return f"{championName} not found in the game."
+    gameName = currPlayer.get("riotIdGameName", '')
+    tagLine = currPlayer.get("riotIdTagLine", '')
+
+    ctx = mcp.get_context()
+    apiKey = ctx.request_context.lifespan_context.apiKey
+    get_puuid_url = f'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}?api_key={apiKey}'
+    async with httpx.AsyncClient() as client:
+        puuid_resp = await client.get(get_puuid_url)
+        puuid_data = puuid_resp.json()
+        puuid = puuid_data['puuid']
+
+    match_history_url = f'https://americas.api.riotgames.com/riot/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=10&api_key={apiKey}'
+    async with httpx.AsyncClient() as client:
+        match_history_resp = await client.get(match_history_url)
+        match_history_data = match_history_resp.json()
+
+    return 'wip'
+  except httpx.RequestError as e:
+    return f"An error occurred while making the request: {e}"
+
+@mcp.tool()
 async def get_current_game_state() -> str:
   """Returns a summary of the current League of Legends game.
   """
